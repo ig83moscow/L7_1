409d5642e7910cf389ab02124609de13f75cb063
Igor Golubev <igor_golubev83@mail.ru>
2021-07-17 13:14:42 +0300

    homework7 is done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+
+    public static void main(String[] args)
+    {
+        Cat[] cats = {
+                new Cat(5, "Дуся"),
+                new Cat(7, "Мурзик"),
+                new Cat(10, "Мышкин")
+        };
+
+        Plate plate = new Plate(20);
+
+        for (Cat cat : cats)
+        {
+            cat.eat(plate);
+        }
+
+        plate.increaseFood(50);
+
+        for (Cat cat : cats)
+        {
+            cat.eat(plate);
+        }
+    }
 }
